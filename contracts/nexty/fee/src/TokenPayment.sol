daa4a1a70019db3eb04bcbf72a47984b742044b5
Zergity <zergity@gmail.com>
2020-08-07 22:24:22 +0700

    contracts: refactor admin var name

--- contracts/nexty/fee/src/TokenPayment.sol
+++ contracts/nexty/fee/src/TokenPayment.sol
@@ -7,37 +7,28 @@ import "./PayerCode.sol";
 
 contract TokenPayment is PayerCode, IConfig {
     bytes32 public constant TRUE = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
-    bytes32 public constant TeamMemberPath = 'TeamMember-'; // bytes11
-    bytes32 public constant TokenForPath = 'TokenFor---'; // bytes11: token for a specific address
+    bytes32 public constant TeamMemberPath  = 'TeamMember-'; // bytes11
+    bytes32 public constant TokenForPath    = 'TokenFor---'; // bytes11: token for a specific address
 
     constructor(
-        address[]   memory team,    // trusted team to set the token prices
-        address[]   memory tokens,  // intitial token addresses
-        uint[]      memory prices   // intitial token prices
+        address[]   memory admins,    // trusted team to set the token prices
+        address[]   memory tokens,  // initial mapping(token address => price) keys
+        uint[]      memory prices   // initial mapping(token address => price) values
     ) public {
-        for (uint i = 0; i < team.length; i++) {
-            addTeamMember(team[i]);
+        for (uint i = 0; i < admins.length; i++) {
+            _addAdmin(admins[i]);
         }
         for (uint i = 0; i < tokens.length; i++) {
             _setPrice(tokens[i], prices[i]);
         }
     }
 
-    function addTeamMember(address member) internal {
+    function _addAdmin(address member) internal {
         ds.store(ds.key11a(TeamMemberPath, member), TRUE);
     }
 
-    function isTeamMember(address member) internal view returns (bool) {
+    function _isAdmin(address member) internal view returns (bool) {
         return ds.load(ds.key11a(TeamMemberPath, member)) != 0;
-    }
-
-    function setTokenFor(address to, address token) external {
-        require(isTeamMember(msg.sender), "for team member only");
-        ds.storeAddress(ds.key11a(TokenForPath, to), token);
-    }
-
-    function _getTokenFor(address to) internal view returns (address) {
-        return ds.loadAddress(ds.key11a(TokenForPath, to));
     }
 
     function getTokenFor(address to) external override view returns (address token) {
@@ -48,12 +39,25 @@ contract TokenPayment is PayerCode, IConfig {
         return to;
     }
 
+    function setTokenFor(address to, address token) external {
+        require(_isAdmin(msg.sender), "for team member only");
+        _setTokenFor(to, token);
+    }
+
+    function _getTokenFor(address to) internal view returns (address) {
+        return ds.loadAddress(ds.key11a(TokenForPath, to));
+    }
+
+    function _setTokenFor(address to, address token) internal {
+        ds.storeAddress(ds.key11a(TokenForPath, to), token);
+    }
+
     function getPrice(address token) external override view returns (uint price) {
         return uint(ds.load(ds.key11a(TokenPricePath, token)));
     }
 
     function setPrice(address token, uint price) external {
-        require(isTeamMember(msg.sender), "for team member only");
+        require(_isAdmin(msg.sender), "for team member only");
         _setPrice(token, price);
     }
 
